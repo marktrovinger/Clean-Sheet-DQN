6eb835f2f2c184c8803b299804290d239a63ad2b
Mark Trovinger <mark.trovinger@gmail.com>
2020-09-11 10:54:28 -0400

    expanded network and started agent class

---
+++ dqn_agent.py
@@ -0,0 +1,6 @@
+from dqn_network import DeepQNetwork
+from replay_buffer import ReplayBuffer
+
+class DQN_Agent():
+    def __init__(self, epsilon, gamma):
+        pass
--- dqn_network.py
+++ dqn_network.py
@@ -3,5 +3,12 @@ import torch.functional as F
 from replay_buffer import ReplayBuffer
 
 class DeepQNetwork(nn.Module):
-    def __init__(self, gamma):
-        super.__init__(DeepQNetwork)
+    def __init__(self, gamma, input_size, epsilon, n_actions):
+        super.__init__(DeepQNetwork, self)
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+        #self.replay_buffer = ReplayBuffer(memory_size)
+        
+        self.input_layer = nn.Linear(input_size, 32)
+        self.conv1 = nn.Conv2d(32, 64, )
