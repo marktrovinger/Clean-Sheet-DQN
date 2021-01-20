a56474353a6e31598b8eb58af88c3aba88024c9a
Mark Trovinger <mark.trovinger@gmail.com>
2021-01-20 16:48:45 -0500

    getting preprocess ready for testing

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
 import gym
 import numpy as np
+import collections
 
 class RepeatActionAndMaxFrame(gym.Wrapper):
     def __init__(self, env, repeat=4):
         super.__init__(RepeatActionAndMaxFrame, self)
+        self.env = env
         self.frame_buffer = np.zeros(env.observation_space * 2)
         self.repeat = repeat
 
@@ -13,8 +15,8 @@ class RepeatActionAndMaxFrame(gym.Wrapper):
         max_frame = 0
 
         for i in range(self.repeat):
-            observation, reward, done, info = env.step(action)
-
+            observation, reward, done, info = self.env.step(action)
+            self.frame_buffer[i] = observation
             total_reward += reward
 
             if done:
@@ -23,7 +25,7 @@ class RepeatActionAndMaxFrame(gym.Wrapper):
         return max_frame, total_reward, done, info
 
     def reset(self):
-        observation = env.reset()
+        observation = self.env.reset()
         self.frame_buffer[0] = observation
 
         return observation
@@ -32,6 +34,7 @@ class PreprocessFrame(gym.ObservationWrapper):
     def __init__(self, env, new_shape):
         super.__init__(PreprocessFrame, self)
         self.new_shape = new_shape
+        self.env = env
 
     def observation(self, observation):
         # convert obs to grey scale
@@ -47,8 +50,39 @@ class PreprocessFrame(gym.ObservationWrapper):
 class StackFrames(gym.ObservationWrapper):
     def __init__(self, env, stack_size):
         super.__init__(StackFrames, self)
+        self.env = env
+        self.stack = collections.deque(maxlen=stack_size)
 
 
     def reset(self):
+        # clear the stack
+        self.stack.clear()
         
+        # reset the env
+        obs = self.env.reset()
+
+        for i in range(stack_size):
+            self.stack.append(obs)
+        stack = np.asarray(list(self.stack))
+        stack = np.reshape(stack, self.env.observation_space.low)
+        return stack
+
+    def observation(self, observation):
+        self.stack.append(observation)
+        stack = np.asarray(list(self.stack))
+        stack = np.reshape(stack, self.env.observation_space.low)
+        return stack
+
+
+def make_env(env_name, new_shape, stack_size):
+    env = gym.make(env_name)
+    env = RepeatActionAndMaxFrame(env)
+    env = PreprocessFrame(env, new_shape)
+    env = StackFrames(env, stack_size)
+
+    return env
+
+
+
+
 
